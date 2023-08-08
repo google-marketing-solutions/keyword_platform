c39a3e18c9947c3b139fd363cdef2875fd7627d6
Maximilian Weiss <maximilianw@google.com>
2023-08-08 06:37:31 -0700

    No public description
    PiperOrigin-RevId: 554806067
    Change-Id: If184bf86e9cb4f72913f592fa571212cf90e62c6

---
+++ py/common/palm_client.py
@@ -0,0 +1,79 @@
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Defines the PalmClient class.
+
+See class doctring for more details.
+"""
+import ast
+import google.generativeai as genai
+
+_MODEL = 'models/text-bison-001'
+
+AVAILABLE_LANGUAGES = frozenset(['en', 'es', 'ko', 'hi', 'zh'])
+
+
+class PalmClient:
+  """A client to make requests to the Palm API.
+
+  This requires an api_key from the Cloud Platform Console.
+
+  Example usage:
+    palm_client = PalmClient(api_key)
+    shortened_text = palm_client.shorten_text_to_char_limit(
+      'Some long headline...', 'en', 50)
+  """
+
+  def __init__(self, api_key: str):
+    self._client = genai.configure(api_key=api_key)
+
+  def shorten_text_to_char_limit(
+      self, text_list: list[str], language_code: str, char_limit: int
+  ) -> list[str]:
+    """Shortens a list of strings under the provided character limit.
+
+    If a text list element is equal or under the character limit already, the
+    original list element will be returned.
+
+    Args:
+      text_list: A list of strings to shorten.
+      language_code: The language of the text.
+      char_limit: The character limit to shorten the text to.
+
+    Returns:
+      The a list of strings that are under the passed character limit.
+
+    Raises:
+      ValueError: If the requested language is not supported.
+    """
+    if language_code in AVAILABLE_LANGUAGES:
+      shorten_prompt = f"""
+        For each of the following list elements, shorten the sentences to be
+        under {char_limit} characters only if it is above {char_limit}
+        characters, retain the given sentence otherwise:
+
+          {text_list}
+
+        Return just a python list.
+      """
+
+      response = genai.generate_text(
+          model=_MODEL,
+          prompt=shorten_prompt,
+          candidate_count=1,
+          temperature=0,
+      )
+      return ast.literal_eval(response.result)
+    else:
+      raise ValueError(f'Language "{language_code}" not supported.')
---
+++ py/common/palm_client_test.py
@@ -0,0 +1,82 @@
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from unittest import mock
+
+import google.generativeai as genai
+
+from absl.testing import absltest
+from common import palm_client as palm_client_lib
+
+_MODEL = 'models/text-bison-001'
+
+
+class PalmClientTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.enter_context(mock.patch.object(genai, 'configure', autospec=True))
+    mock_response = mock.MagicMock()
+    mock_response.result = (
+        "['This texts needs to be shortened because it is too long.']"
+    )
+    self._mock_generate_text = self.enter_context(
+        mock.patch.object(
+            genai, 'generate_text', autospec=True, return_value=mock_response
+        )
+    )
+
+  def test_shorten_text_to_char_limit(self):
+    fake_text_list = [
+        'This texts needs to be shortened because it is too long.',
+    ]
+    test_char_limit = 50
+    expected_prompt = f"""
+        For each of the following list elements, shorten the sentences to be
+        under {test_char_limit} characters only if it is above {test_char_limit}
+        characters, retain the given sentence otherwise:
+
+          {fake_text_list}
+
+        Return just a python list.
+      """
+    expected_result = [
+        'This texts needs to be shortened because it is too long.'
+    ]
+    palm_client = palm_client_lib.PalmClient('api_key')
+
+    actual_result = palm_client.shorten_text_to_char_limit(
+        fake_text_list, 'en', 50
+    )
+
+    self._mock_generate_text.assert_called_once_with(
+        model=_MODEL,
+        prompt=expected_prompt,
+        candidate_count=1,
+        temperature=0,
+    )
+    self.assertEqual(actual_result, expected_result)
+
+  def test_shorten_text_to_char_limit_raises_value_error_unsupported_language(
+      self,
+  ):
+    palm_client = palm_client_lib.PalmClient('api_key')
+
+    with self.assertRaises(ValueError):
+      # Language is not listed under the palm_client_lib.AVAILABLE_LANGUAGES.
+      palm_client.shorten_text_to_char_limit(['some_text'], 'unsupported', 50)
+
+
+if __name__ == '__main__':
+  absltest.main()
