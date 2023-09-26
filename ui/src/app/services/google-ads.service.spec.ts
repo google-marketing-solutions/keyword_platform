efdce99191781ef2271a2d5710b50998f63c15eb
David Solis <dsolis@google.com>
2023-09-26 14:56:27 -0700

    No public description
    PiperOrigin-RevId: 568657249
    Change-Id: Ibfc00cf0544744b8de66a4cfbda4b34be4ccc2b4

--- ui/src/app/services/google-ads.service.spec.ts
+++ ui/src/app/services/google-ads.service.spec.ts
@@ -15,21 +15,76 @@
  * limitations under the License.
  */
 
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HttpResponse} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
+
+import {LOCATION_TOKEN} from '../shared/tokens';
 
 import {GoogleAdsService} from './google-ads.service';
 
 describe('GoogleAdsService', () => {
   let service: GoogleAdsService;
+  let httpMock: HttpTestingController;
+  let getAccountsSpy: jasmine.Spy;
+  let getCampaignsSpy: jasmine.Spy;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [HttpClientModule]});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [{
+        provide: LOCATION_TOKEN,
+        useValue: {hostname: 'hostname.0.0.0.0.nip.io'}
+      }],
+
+    });
     service = TestBed.inject(GoogleAdsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  // TODO(): Add effective unit tests.
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should use http GET method with getAccounts', () => {
+    service.getAccounts().subscribe();
+    const request = httpMock.expectOne('./proxy?endpoint=accessible_accounts');
+    expect(request.request.method).toBe('GET');
+    httpMock.verify();
+  });
+
+  it('should respond successfully using getAccounts', () => {
+    const body = [{'id': '1', 'name': 'Account 1'}];
+    const httpResponse =
+        new HttpResponse({body, status: 200, statusText: 'OK'});
+    getAccountsSpy = spyOn(TestBed.inject(GoogleAdsService), 'getAccounts');
+    getAccountsSpy.and.returnValue(of(httpResponse));
+    service.getAccounts().subscribe(response => {
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe('OK');
+      expect(response.body).toBe(body);
+    });
+  });
+
+  it('should use http GET method with getCampaigns', () => {
+    service.getCampaigns(['1']).subscribe();
+    const request =
+        httpMock.expectOne('./proxy?selected_accounts=1&endpoint=campaigns');
+    expect(request.request.method).toBe('GET');
+    httpMock.verify();
+  });
+
+  it('should respond successfully using getCampaigns', () => {
+    const body = [{'id': '1', 'name': 'Campaign 1'}];
+    const httpResponse =
+        new HttpResponse({body, status: 200, statusText: 'OK'});
+    getCampaignsSpy = spyOn(TestBed.inject(GoogleAdsService), 'getCampaigns');
+    getCampaignsSpy.and.returnValue(of(httpResponse));
+    service.getCampaigns(['1']).subscribe(response => {
+      expect(response.status).toBe(200);
+      expect(response.statusText).toBe('OK');
+      expect(response.body).toBe(body);
+    });
   });
 });
