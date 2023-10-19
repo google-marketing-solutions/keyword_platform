f7e65696177b41e6fc75faeacc57257f31ce0e9b
Maximilian Weiss <maximilianw@google.com>
2023-10-19 09:34:32 -0700

    No public description
    PiperOrigin-RevId: 574900608
    Change-Id: I395c00a5c3dafc2b649d5b014956f9d509e2eea9

--- py/common/execution_analytics_client.py
+++ py/common/execution_analytics_client.py
@@ -100,10 +100,7 @@ class ExecutionAnalyticsClient:
         }],
     }
     if self._is_valid_hit(qs):
-      response = requests.post(self._url, json=qs)
-      if response.status_code != 200:
-        logging.error('GA4 hit failed to send: %s', json.loads(response.text))
-      return response
+      return requests.post(self._url, json=qs)
 
   def _is_valid_hit(self, qs: dict[str, Any]) -> bool:
     """Returns True if the hit is valid.
@@ -114,7 +111,7 @@ class ExecutionAnalyticsClient:
     is_valid_hit = True
     debug_response = requests.post(self._debug_url, json=qs)
     validation_messages = json.loads(debug_response.text)['validationMessages']
-    if (validation_messages or debug_response.status_code != 200):
+    if validation_messages:
       logging.debug('GA4 hit not valid: %s', validation_messages)
       is_valid_hit = False
     return is_valid_hit
