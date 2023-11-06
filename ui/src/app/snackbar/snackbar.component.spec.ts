1a6e71be4292c6871dad74fb46ca64104eee9066
David Solis <dsolis@google.com>
2023-11-06 06:58:41 -0800

    No public description
    PiperOrigin-RevId: 579830536
    Change-Id: I8cffdba0aba29cb91a1c33ac85f006ae3ca7aeee

---
+++ ui/src/app/snackbar/snackbar.component.spec.ts
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {MatSnackBarRef, MAT_SNACK_BAR_DATA} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+
+import { SnackbarComponent } from './snackbar.component';
+
+describe('SnackbarComponent', () => {
+  let component: SnackbarComponent;
+  let fixture: ComponentFixture<SnackbarComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SnackbarComponent],
+      imports: [MatIconModule],
+      providers: [
+        {provide: MAT_SNACK_BAR_DATA, useValue: {}},
+        {provide: MatSnackBarRef, useFactory: (()=>{})}
+      ],
+    });
+    fixture = TestBed.createComponent(SnackbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  // TODO(): Add effective unit tests.
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
